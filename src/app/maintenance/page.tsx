743ed71de76ef8c7f896945d223fb1dc2396b0fb
Jesse Olisa <jesse@instantotp.com>
2024-10-18 14:02:55 +0100

    feat/fix-page

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -2,28 +2,26 @@ import { buttonVariants } from '@/components/ui/button';
 
 export default function MaintenancePage() {
   return (
-    <main className='w-full grid place-items-center h-svh'>
-      <div className='w-[811px] flex justify-center  items-center bg-[#ecf4fb] rounded-[20px] py-20'>
-        <div className='space-y-12'>
+    <main className='w-full grid place-items-center h-svh container'>
+      <div className='max-w-[811px] flex justify-center  items-center bg-[#ecf4fb] rounded-[20px] py-20'>
+        <div className='space-y-12 px-8'>
           <div className='space-y-6'>
-            <div className='text-center text-[#001943] text-[35px] font-semibold '>
-              Sorry, we&apos;re down for maintance in
+            <div className='text-center text-[#001943] text-lg lg:text-[35px] font-semibold font-eudoxusSans '>
+              Sorry, we&apos;re down for maintance
             </div>
             <div>
-              <div className='text-center'>
+              <div className='text-center font-eudoxusSans text-sm lg:text-base'>
                 We expect to be back in couple of hours. Thanks for your
                 patience.
               </div>
             </div>
           </div>
-          <div className='w-full max-w-[560px] h-[50px] px-8 py-4 bg-[#001943] rounded-[10px] justify-center items-center gap-[15px] inline-flex'>
-            <a
-              href='https://www.etegram.com/'
-              className={buttonVariants()}
-            >
-              OK
-            </a>
-          </div>
+          <a
+            href='https://www.etegram.com/'
+            className={buttonVariants({ className: 'w-full' })}
+          >
+            OK
+          </a>
         </div>
       </div>
     </main>
