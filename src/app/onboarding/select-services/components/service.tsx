32be067ead4d174a98f9d99478ada798b77f3569
Jesse Olisa <jesse@instantotp.com>
2024-10-21 10:48:13 +0100

    feat/update

--- src/app/onboarding/select-services/components/service.tsx
+++ src/app/onboarding/select-services/components/service.tsx
@@ -13,18 +13,18 @@ export type selectServicesType = {
   name: string;
   description: string;
   id: string;
-  status: 'active' | 'inactive';
-  icon: IconType | string;
+  status: boolean;
+  icon: string;
 };
 
 const moreServices: selectServicesType[] = [
   {
-    icon: RiHandCoinFill,
+    icon: '/assets/icons/QR Code Payments.svg',
     id: '72678',
     name: 'Collect Payment',
     description:
       'Easily collect payments online with various options like virtual accounts and payment modal!',
-    status: 'active',
+    status: true,
   },
   {
     icon: '/assets/icons/QR Code Payments.svg',
@@ -32,7 +32,7 @@ const moreServices: selectServicesType[] = [
     name: 'QR code Payments',
     description:
       'Accept payment easily and effortlessly in your physcial store by displaying your contactless QR code.',
-    status: 'inactive',
+    status: false,
   },
   {
     icon: '/assets/icons/Corporate Account.svg',
@@ -40,7 +40,7 @@ const moreServices: selectServicesType[] = [
     name: 'Corporate Account',
     description:
       'Separate your personal finances from your business finances with the etegram business bank account',
-    status: 'inactive',
+    status: false,
   },
   {
     icon: '/assets/icons/Payment Links.svg',
@@ -48,7 +48,7 @@ const moreServices: selectServicesType[] = [
     name: 'Payment links',
     description:
       'Easily sell online without a website using our payment link feature',
-    status: 'inactive',
+    status: false,
   },
 ];
 
@@ -57,6 +57,7 @@ export default function ServicesList({
 }: {
   services: selectServicesType[];
 }) {
+  // console.log(services);
   const [value, setValue] = useState('');
   return (
     <div>
@@ -72,7 +73,7 @@ export default function ServicesList({
             key={item.id}
             className='flex relative justify-between space-x-3 p-4 items-center  rounded-[0.625rem] border-[1.5px] border-border w-full  peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary'
           >
-            {item.status !== 'active' && (
+            {item?.status && (
               <p className='text-destructive absolute right-4 top-3.5 text-xs'>
                 Coming Soon
               </p>
@@ -81,10 +82,10 @@ export default function ServicesList({
               htmlFor={item.name}
               className={cn(
                 'max-w-[25rem] flex space-x-6 items-center cursor-pointer',
-                { 'opacity-75 cursor-not-allowed': item.status !== 'active' }
+                { 'opacity-75 cursor-not-allowed': item?.status }
               )}
             >
-              {typeof item.icon === 'string' ? (
+              {/* {typeof item.icon === 'string' ? (
                 <div className='flex justify-center items-center bg-accent p-2 rounded-full w-[60px] h-[60px] flex-shrink-0'>
                   <Image
                     src={item.icon ?? '/assets/icons/QR Code Payments.svg'}
@@ -98,7 +99,17 @@ export default function ServicesList({
                 <div className='flex justify-center items-center bg-accent p-2 rounded-full w-[60px] h-[60px] flex-shrink-0'>
                   <item.icon className='w-[30px] h-[30px]' />
                 </div>
-              )}
+              )} */}
+              <div className='flex justify-center items-center bg-accent p-2 rounded-full w-[60px] h-[60px] flex-shrink-0'>
+                <Image
+                  src={item.icon ?? '/assets/icons/QR Code Payments.svg'}
+                  alt='Invoice'
+                  width={30}
+                  height={30}
+                  quality={100}
+                />
+              </div>
+
               <div className='space-y-1'>
                 <p className='text-base font-medium'>{item.name}</p>
                 <p className='text-primary/50 text-sm'>{item.description}</p>
@@ -108,7 +119,7 @@ export default function ServicesList({
               value={item.id}
               id={item.name}
               className='flex-shrink-0'
-              disabled={item.status !== 'active'}
+              disabled={item?.status}
             />
           </div>
         ))}
